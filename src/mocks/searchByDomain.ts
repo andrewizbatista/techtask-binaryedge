432cf74a5d56941f2734aa77be5132467dc1fa7b
André Batista <hi@andrebatista.dev>
2020-04-07 14:00:59 +0100

    add mocked data

---
+++ src/mocks/searchByDomain.ts
@@ -0,0 +1,123 @@
+const searchByDomain = [
+  {
+    name: 'dataleak1',
+    emails: [
+      {
+        email: 'pay_mobile@example.com',
+      },
+      {
+        email: 'library_mistake@example.com',
+      },
+      {
+        email: 'statement_cat@example.com',
+      },
+      {
+        email: 'move_chest@example.com',
+      },
+      {
+        email: 'advantage_cell@example.com',
+      },
+      {
+        email: 'limit_mission@example.com',
+      },
+      {
+        email: 'two_natural@example.com',
+      },
+      {
+        email: 'charity_point@example.com',
+      },
+      {
+        email: 'exchange_bedroom@example.com',
+      },
+      {
+        email: 'parking_marriage@example.com',
+      },
+      {
+        email: 'private_split@example.com',
+      },
+      {
+        email: 'essay_touch@example.com',
+      },
+      {
+        email: 'advice_influence@example.com',
+      },
+      {
+        email: 'bedroom_sell@example.com',
+      },
+      {
+        email: 'wealth_unit@example.com',
+      },
+      {
+        email: 'development_crash@example.com',
+      },
+      {
+        email: 'still_green@example.com',
+      },
+      {
+        email: 'mission_employ@example.com',
+      },
+    ],
+  },
+
+  {
+    name: 'dataleak2',
+    emails: [
+      {
+        email: 'pay_mobile@example.com',
+      },
+      {
+        email: 'library_mistake@example.com',
+      },
+      {
+        email: 'statement_cat@example.com',
+      },
+      {
+        email: 'move_chest@example.com',
+      },
+      {
+        email: 'advantage_cell@example.com',
+      },
+      {
+        email: 'limit_mission@example.com',
+      },
+      {
+        email: 'two_natural@example.com',
+      },
+      {
+        email: 'charity_point@example.com',
+      },
+      {
+        email: 'exchange_bedroom@example.com',
+      },
+      {
+        email: 'parking_marriage@example.com',
+      },
+      {
+        email: 'private_split@example.com',
+      },
+      {
+        email: 'essay_touch@example.com',
+      },
+      {
+        email: 'advice_influence@example.com',
+      },
+      {
+        email: 'bedroom_sell@example.com',
+      },
+      {
+        email: 'wealth_unit@example.com',
+      },
+      {
+        email: 'development_crash@example.com',
+      },
+      {
+        email: 'still_green@example.com',
+      },
+      {
+        email: 'mission_employ@example.com',
+      },
+    ],
+  },
+];
+
+export default searchByDomain;
---
+++ src/mocks/searchByEmail.ts
@@ -0,0 +1,16 @@
+const searchByEmail = [
+  {
+    name: 'dataleak1',
+  },
+  {
+    name: 'dataleak_abc',
+  },
+  {
+    name: 'dataleak_123',
+  },
+  {
+    name: 'dataleak_gg12',
+  },
+];
+
+export default searchByEmail;
