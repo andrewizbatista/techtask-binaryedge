5bc75f63032cee7e7c1050522e87291f5d7a7301
André Batista <hi@andrebatista.dev>
2020-04-08 15:28:01 +0100

    fix issues with the storybook

--- components/InputField/index.stories.tsx
+++ components/InputField/index.stories.tsx
@@ -8,4 +8,28 @@ export default {
   parameters: {},
 };
 
-export const Basic = () => <InputField />;
+export const Basic = () => (
+  <InputField
+    type="text"
+    name="basic"
+    label="Basic Example"
+    value=""
+    error={undefined}
+    touch={false}
+    handleBlur={() => true}
+    handleChange={() => true}
+  />
+);
+
+export const WithError = () => (
+  <InputField
+    type="text"
+    name="basic"
+    label="With Error Example"
+    value=""
+    error="This is an error message."
+    touch
+    handleBlur={() => true}
+    handleChange={() => true}
+  />
+);
